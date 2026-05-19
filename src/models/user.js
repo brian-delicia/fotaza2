ba05a4c7660419d374d10a420858b611ac28bf4b
brian-delicia <briandaviddelicia@gmail.com>
2026-05-18 21:37:53 -0300

    modelo de la tabla User

---
+++ src/models/user.js
@@ -0,0 +1,41 @@
+const{Model,DataTypes}=require('sequelize');
+const sequelize= require('../database')
+
+
+class User extends Model {}
+
+User.init({
+    name:{
+        Type:DataTypes.STRING(60),
+        allowNull:false
+    },
+    email:{
+        type:DataTypes.STRING(60),
+        allowNull:false,
+        unique:true
+    },
+    password:{
+        type:DataTypes.STRING(60),
+        allowNull:false,
+      
+
+    },
+    role:{
+        type:DataTypes.STRING(60),
+        defaultValue: 'user'
+    },
+    active:{
+        type:DataTypes.BOOLEAN,
+        defaultValue:true
+    }
+},{
+    sequelize,
+    modelName:'User',
+    tableName:'users',
+    timestamps:true,
+    createdAt:'created_at',
+    deletedAt:true
+
+
+});
+module.exports=User
