a9efc4002f21a6bcf6b8d52f14baf417529a6e40
brian-delicia <briandaviddelicia@gmail.com>
2026-06-02 14:02:44 -0300

    controller image y relaciones de tabla

---
+++ src/controllers/image.controller.js
@@ -0,0 +1,104 @@
+const {User,Post,Image,Comment,Rating }=require('../models');
+
+exports.showRaw = async (req,res)=>{    //mostrar original 
+  try {
+    
+    const image= await Image.findByPk(req.params.imageId)
+   
+    if(!image){
+        res.status(404).send('imagen no encontrada')
+        return
+     }
+     res.set('content-type',image.mime_type); //LE DICE AL NAVEGADOR QUE TIPO DE IMAGEN RECIVE
+     res.send(image.image_data);     //ENVIA LOS DATO BINARIOS DE LA IMAGEN
+     return
+
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('eror del servidor al mostrar la imagen ')
+
+    
+  }
+}
+
+exports.detail = async (req,res)=>{
+    try {
+        const image= await Image.findByPk(req.params.imageId,{
+            attributes:['id','license','watermark_text','mime_type','comments_open',
+                'created_at'],
+                include:[{
+                    model:Post,
+                    include:[{
+                        model:User,
+                        attributes:['id','name']
+                    }]
+                },{
+                    model:Comment,
+                    where:{active:true},
+                    required:false,
+                    include:[{
+                        model:User,
+                        attributes:['id','name']
+                    }]
+                },{
+                    model:Rating,
+                    attributes:['value']
+                }
+            ]
+        });
+        if(!image){
+            res.render('errors/404');
+            return
+        }
+
+        const ratings= image.Ratings || [];
+
+        let ratingAverage=0;
+        if(ratings.length>0){
+            const total =ratings.reduce((sum,rating)=>{
+               return sum+rating.value
+                 },0)
+                ratingAverage= total / ratings.length;
+        }
+        res.render('image/detail',{
+            image,
+            comments:image.Comments || [],
+            ratingAverage,
+            ratingCount:ratings.length
+            
+        }
+    );  return
+
+
+    } catch (error) {
+        console.error(error)
+        res.render('errors/500');
+        return    
+    }
+}
+exports.closeComments = async (req,res)=>{
+    try {
+        const image= await Image.findByPk(req.params.imageId,{
+            include:[Post]
+        })
+        if(!image){
+            res.redirect('/posts')
+            return
+        }
+        if(image.Post.user_id!==req.session.user.id){
+            res.redirect(`/images/${image.id}`)
+            return
+        }
+
+        await image.update({
+            comments_open:false
+        })
+        res.redirect(`/images/${image.id}`)
+
+    } catch (error) {
+        console.error(error)
+        res.redirect('posts')
+        return
+    }
+}
--- src/models/index.js
+++ src/models/index.js
@@ -16,6 +16,7 @@ const Message = require('./Message');
 
 
 
+
 //un usuario puede tener muchos post y un post es de un usuario
 User.hasMany(Post,{foreignKey:'user_id'});
 Post.belongsTo(User,{foreignKey:'user_id'})
@@ -24,17 +25,117 @@ Post.belongsTo(User,{foreignKey:'user_id'})
 Post.hasMany(Image,{foreignKey:'post_id'})
 Image.belongsTo(Post,{foreignKey:'post_id'})
 
-//un post puede tener muchos comentarios y un comentario es de un solo post
-Post.hasMany(Comment,{foreignKey:'post_id'})
-Comment.belongsTo(Post,{foreignKey:'post_id'})
+//publicaciones pueden teneer muchas etiquetas y una etiqueta mucha publiciones 
+Post.belongsToMany(Tag,{through:PostTag,foreignKey:'post_id'})
+Tag.belongsToMany(Post,{through:PostTag,foreignKey:'tag_id'})
 
-//un User puede tener muchos comment y un comment puede ser de un User
+//imagen puede tener muchos comentarios y mucho comntarios a la misma imagen 
+Image.hasMany(Comment,{foreignKey:'image_id'})
+Comment.belongsTo(Image,{foreignKey:'image_id'})
+
+//un User puede tener muchos comment y muchos comment puede ser de un User
 User.hasMany(Comment,{foreignKey:'user_id'})
 Comment.belongsTo(User,{foreignKey:'user_id'})
 
-module.exports={
-    User,
-    Image,
-    Post,
-    Comment
-}
+//una imagen puede tener muchas valoraciones y mucha valoraciones pueden pertencer a una imagn  
+Image.hasMany(Rating,{foreignKey:'image_id'})
+Rating.belongsTo(Image,{foreignKey:'image_id'})
+
+//usuario puede tener muchas valoraciones y muchas valoraciones puden ser de  un usuario 
+User.hasMany(Rating,{foreignKey:'user_id'})
+Rating.belongsTo(User,{foreignKey:'user_id'})
+
+//un usuario puedetener muchos seguidores 
+User.belongsToMany(User,{
+    through:Follow,
+    as:'following',
+    foreignKey:'follower_id',
+    otherKey:'followed_id'
+});
+User.belongsToMany(User,{
+    through:Follow,
+    as:'followers',
+    foreignKey:'followed_id',
+    otherKey:'follower_id'
+
+})
+
+// Notificaciones
+User.hasMany(Notification, {foreignKey: 'user_id'});
+
+Notification.belongsTo(User, {foreignKey: 'user_id'});
+
+Notification.belongsTo(User, {foreignKey: 'actor_id',
+     as: 'actor'});
+
+// Colecciones del usuario
+User.hasMany(Collection, { foreignKey: 'user_id'});
+
+Collection.belongsTo(User, {foreignKey: 'user_id'});
+
+// Colecciones - Publicaciones
+Collection.belongsToMany(Post, {through: CollectionPost,foreignKey: 'collection_id'});
+
+Post.belongsToMany(Collection, {through: CollectionPost,foreignKey: 'post_id'});
+
+// Colecciones - Imagenes
+Collection.belongsToMany(Image, {through: CollectionImage,foreignKey: 'collection_id'});
+
+Image.belongsToMany(Collection, {through: CollectionImage,foreignKey: 'image_id'});
+
+// Denuncias de imagenes
+User.hasMany(Report, {foreignKey: 'user_id'});
+
+Report.belongsTo(User, {foreignKey: 'user_id'});
+
+Image.hasMany(Report, { foreignKey: 'image_id'});
+
+Report.belongsTo(Image, { foreignKey: 'image_id'});
+
+// Denuncias de comentarios
+Comment.hasMany(Report, { foreignKey: 'comment_id'});
+
+Report.belongsTo(Comment, {foreignKey: 'comment_id'});
+
+// Me interesa
+User.hasMany(Interest, { foreignKey: 'user_id'});
+
+Interest.belongsTo(User, {foreignKey: 'user_id'});
+
+Image.hasMany(Interest, {foreignKey: 'image_id'});
+
+Interest.belongsTo(Image, { foreignKey: 'image_id'});
+
+// Mensajes privados
+Interest.hasMany(Message, {foreignKey: 'interest_id'});
+
+Message.belongsTo(Interest, { foreignKey: 'interest_id'});
+
+User.hasMany(Message, { foreignKey: 'sender_id', as: 'sentMessages'});
+
+Message.belongsTo(User, {foreignKey: 'sender_id', as: 'sender'});
+
+User.hasMany(Message, {foreignKey: 'receiver_id',as: 'receivedMessages'});
+
+Message.belongsTo(User, { foreignKey: 'receiver_id', as: 'receiver'});
+
+
+
+
+module.exports = {
+  User,
+  Post,
+  Image,
+  Tag,
+  PostTag,
+  Comment,
+  Rating,
+  Follow,
+  Notification,
+  Collection,
+  CollectionPost,
+  CollectionImage,
+  Report,
+  Interest,
+  Message
+};
