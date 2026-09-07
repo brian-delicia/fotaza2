fa9a902f14c7d329b35541a9e4c45920507ccfca
brian-delicia <briandaviddelicia@gmail.com>
2026-09-07 14:51:27 -0300

    confgurar ssl para postgreSQL

--- src/database.js
+++ src/database.js
@@ -1,16 +1,29 @@
 const Sequelize = require ('sequelize');
 require('dotenv').config();
 
-const sequelize=new Sequelize({
+const sequelize = new Sequelize({
     database: process.env.DB_NAME,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect:'postgres'
+    dialect: 'postgres',
 
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
-
 module.exports=sequelize;
 console.log('DB_NAME:', process.env.DB_NAME);
 console.log('DB_HOST:', process.env.DB_HOST);
+
+sequelize.authenticate()
+    .then(() => {
+        console.log(' Conexión a PostgreSQL exitosa');
+    })
+    .catch((error) => {
+        console.error(' Error de conexión:', error.message);
+    });
